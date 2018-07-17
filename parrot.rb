e2fb8c0bace7c679a02c5f95b724cb2d2d1a5e93
Ahmadali <ahmadali.azizov@outlook.com>
2018-07-17 19:35:25 +0000

    automatically backed up by learn

--- parrot.rb
+++ parrot.rb
@@ -1,9 +1,8 @@
 require 'pry'
 def parrot(phrase="Squawk!")
 puts "#{phrase}"
-binding.pry
 end
 parrot("Pretty bird!")
-
+binding.pry
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
